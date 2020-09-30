82d40be9b9a5860265333addebac1967353ce222
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-09-30 18:40:18 +0530

    Update fedutl.py

--- userbot/plugins/fedutl.py
+++ userbot/plugins/fedutl.py
@@ -61,7 +61,45 @@ async def _(event):
             except YouBlockedUserError:
                 await event.edit("**Error:** `unblock` @MissRose_Bot `and try again!")
 
-
+@borg.on(admin_cmd("roseinfo ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    sysarg = event.pattern_match.group(1)
+    if sysarg == "":
+        async with borg.conversation(bot) as conv:
+            try:
+                await conv.send_message("/start")
+                await conv.get_response()
+                await conv.send_message("/info")
+                audio = await conv.get_response()
+                await borg.send_message(event.chat_id, audio.text)
+                await event.delete()
+            except YouBlockedUserError:
+                await event.edit("**Error:** `unblock` @MissRose_bot `and retry!")
+    elif "@" in sysarg:
+        async with borg.conversation(bot) as conv:
+            try:
+                await conv.send_message("/start")
+                await conv.get_response()
+                await conv.send_message("/info " + sysarg)
+                audio = await conv.get_response()
+                await borg.send_message(event.chat_id, audio.text)
+                await event.delete()
+            except YouBlockedUserError:
+                await event.edit("**Error:** `unblock` @MissRose_Bot `and try again!")
+    elif "" in sysarg:
+        async with borg.conversation(bot) as conv:
+            try:
+                await conv.send_message("/start")
+                await conv.get_response()
+                await conv.send_message("/info " + sysarg)
+                audio = await conv.get_response()
+                await borg.send_message(event.chat_id, audio.text)
+                await event.delete()
+            except YouBlockedUserError:
+                await event.edit("**Error:** `unblock` @MissRose_Bot `and try again!")
+                
 @borg.on(admin_cmd(pattern=r"plist ?(.*)", outgoing=True))
 async def get_users(show):
     await show.delete()
