3492bb23d336d19a961a8ce89fc1e924df0782f6
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-10-02 11:31:36 +0530

    Update removebg.py

--- userbot/plugins/removebg.py
+++ userbot/plugins/removebg.py
@@ -68,11 +68,12 @@ async def remove_background(event):
     if "image" in contentType:
         if cmd == "rmbg":
             with io.BytesIO(output_file_name.content) as remove_bg_image:
-                remove_bg_image.name = "BG_less.png"
+                remove_bg_image.name = "CATBG_less.png"
                 await borg.send_file(
                     event.chat_id,
                     remove_bg_image,
                     force_document=True,
+                    caption="__**➥ Removed dat annoying Background just for you.**__🥳",
                     reply_to=message_id,
                 )
             await event.delete()
