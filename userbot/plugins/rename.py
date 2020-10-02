26818bc0c5637e1e3fa1656175361e802cc213d5
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-10-02 15:07:10 +0530

    Update rename.py

--- userbot/plugins/rename.py
+++ userbot/plugins/rename.py
@@ -47,7 +47,7 @@ def get_video_thumb(file, output=None, width=90):
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "SurCat"
-USERNAME = str(Config.USERNAME) if Config.USERNAME else "@Surv_ivor"
+SURID = bot.uid
 
 
 @borg.on(admin_cmd(pattern="rename (.*)"))
@@ -78,7 +78,7 @@ async def _(event):
         ms = (end - start).seconds
         if os.path.exists(downloaded_file_name):
             await event.edit(
-                f"__**➥ Downloaded in {ms} seconds.**__\n__**➥ Downloaded to :- **__ `{downloaded_file_name}`\n__**➥ Downloaded by :-**__ [{DEFAULTUSER}]({USERNAME})"
+                f"__**➥ Downloaded in {ms} seconds.**__\n__**➥ Downloaded to :- **__ `{downloaded_file_name}`\n__**➥ Downloaded by :-**__ [{DEFAULTUSER}](tg://user?id={SURID})"
             )
         else:
             await event.edit("Error Occurred\n {}".format(input_str))
@@ -134,7 +134,7 @@ async def _(event):
             ms_two = (end_two - end).seconds
             await event.delete()
             await caat.edit(
-                f"__**➥ Downloaded in {ms_one} seconds.**__\n__**➥ Uploaded in {ms_two} seconds.**__\n__**➥ Uploaded by :-**__ [{DEFAULTUSER}]({USERNAME})"
+                f"__**➥ Downloaded in {ms_one} seconds.**__\n__**➥ Uploaded in {ms_two} seconds.**__\n__**➥ Uploaded by :-**__ [{DEFAULTUSER}](tg://user?id={SURID})"
             )
         else:
             await event.edit("File Not Found {}".format(input_str))
@@ -190,7 +190,7 @@ async def _(event):
             ms_two = (end_two - end).seconds
             await event.delete()
             await caat.reply(
-                f"__**➥ Downloaded in {ms_one} seconds.**__\n__**➥ Uploaded in {ms_two} seconds.**__\n__**➥ Uploaded by :-**__ [{DEFAULTUSER}]({USERNAME})"
+                f"__**➥ Downloaded in {ms_one} seconds.**__\n__**➥ Uploaded in {ms_two} seconds.**__\n__**➥ Uploaded by :-**__ [{DEFAULTUSER}](tg://user?id={SURID})"
             )
         else:
             await event.reply("File Not Found {}".format(input_str))
@@ -281,7 +281,7 @@ async def _(event):
                 ms_two = (end - end_one).seconds
                 await event.delete()
                 await caat.edit(
-                    f"__**➥ Downloaded in {ms_one} seconds.**__\n__**➥ Uploaded in {ms_two} seconds.**__\n__**➥ Uploaded by :-**__ [{DEFAULTUSER}]({USERNAME})"
+                    f"__**➥ Downloaded in {ms_one} seconds.**__\n__**➥ Uploaded in {ms_two} seconds.**__\n__**➥ Uploaded by :-**__ [{DEFAULTUSER}](tg://user?id={SURID})"
                 )
         else:
             await event.edit("File Not Found {}".format(input_str))
@@ -367,7 +367,7 @@ async def _(event):
             ms_two = (end_two - end).seconds
             await event.delete()
             await caat.edit(
-                f"__**➥ Downloaded in {ms_dl} seconds.**__\n__**➥ Uploaded in {ms_two} seconds.**__\n__**➥ Uploaded by :-**__ [{DEFAULTUSER}]({USERNAME})"
+                f"__**➥ Downloaded in {ms_dl} seconds.**__\n__**➥ Uploaded in {ms_two} seconds.**__\n__**➥ Uploaded by :-**__ [{DEFAULTUSER}](tg://user?id={SURID})"
             )
         else:
             await event.edit("File Not Found {}".format(input_str))
