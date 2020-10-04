3aa060605fb6c1d9b803d2ea433ba15de14a595c
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-10-05 00:35:41 +0530

    Update musicdl.py

--- userbot/plugins/musicdl.py
+++ userbot/plugins/musicdl.py
@@ -1,6 +1,6 @@
 """ Spotify / Deezer downloader plugin by @Sur_vivor | Syntax: .dzd link"""
 import asyncio
-
+from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 from userbot.utils import admin_cmd
@@ -61,3 +61,34 @@ async def WooMai(netase):
         await bot.send_file(netase.chat_id, respond)
     await netase.client.delete_messages(conv.chat_id, [msg.id, response.id, respond.id])
     await netase.delete()
+    
+@borg.on(admin_cmd(outgoing=True, pattern="spd(?: |$)(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    chat = "@SpotifyMusicDownloaderBot"
+    await event.edit("```Getting Your Music```")
+    async with bot.conversation(chat) as conv:
+        await asyncio.sleep(2)
+        await event.edit("`Downloading music taking some times,  Stay Tuned.....`")
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=752979930)
+            )
+            msg = await bot.send_message(chat, link)
+            respond = await response
+            res = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=752979930)
+            )
+            r = await res
+            """ - don't spam notif - """
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await event.reply(
+                "```Please unblock @SpotifyMusicDownloaderBot and try again```"
+            )
+            return
+        await bot.forward_messages(event.chat_id, respond.message)
+    await event.client.delete_messages(conv.chat_id, [msg.id, r.id, respond.id])
+    await event.delete()    
