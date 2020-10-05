544253b1971fd404dcce012d9454b0abf21314a0
Sur-vivor <59915404+Sur-vivor@users.noreply.github.com>
2020-10-05 07:36:42 +0000

    pylint: auto fixes

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 from userbot import StartTime, catdef
 
-from .. import CMD_HELP, ALIVE_NAME
+from .. import ALIVE_NAME, CMD_HELP
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "SurCat"
