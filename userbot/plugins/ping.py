5f384ebd75b7b13c7a894fb0cd8172c03cf2c3fe
Sur-vivor <59915404+Sur-vivor@users.noreply.github.com>
2020-10-05 06:51:30 +0000

    pylint: auto fixes

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -2,11 +2,13 @@ import asyncio
 import time
 from datetime import datetime
 
+from userbot import StartTime, catdef
+
 from .. import CMD_HELP
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 
-from userbot import StartTime, catdef
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "SurCat"
+
 
 @borg.on(admin_cmd(pattern="ping$"))
 @borg.on(sudo_cmd(pattern="ping$", allow_sudo=True))
@@ -19,7 +21,9 @@ async def _(event):
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     uptime = await catdef.get_readable_time((time.time() - StartTime))
-    await event.edit(f"__**☞ Pong!__**\n➥__**Ping Speed**__ {ms}\n➥__**Uptime**__ {uptime}\n➥ __**Bot**__ __**of**__ [{DEFAULTUSER}](tg://user?id={SURID})")
+    await event.edit(
+        f"__**☞ Pong!__**\n➥__**Ping Speed**__ {ms}\n➥__**Uptime**__ {uptime}\n➥ __**Bot**__ __**of**__ [{DEFAULTUSER}](tg://user?id={SURID})"
+    )
 
 
 @borg.on(admin_cmd(pattern=f"fping$", outgoing=True))
