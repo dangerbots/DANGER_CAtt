7efe776ef749ab37900e9308dd667aed823f24b0
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-10-11 22:08:00 +0530

    Update getsongs.py

--- userbot/plugins/getsongs.py
+++ userbot/plugins/getsongs.py
@@ -12,8 +12,8 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 from validators.url import url
 
-from ..utils import admin_cmd, edit_or_reply, sudo_cmd, ALIVE_NAME
-from . import CMD_HELP, name_dl, runcmd, song_dl, video_dl, yt_search
+from ..utils import admin_cmd, edit_or_reply, sudo_cmd
+from . import CMD_HELP, name_dl, runcmd, song_dl, video_dl, yt_search, ALIVE_NAME
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "SurCat"
 SURID = bot.uid
