8121a65bb82bf613758227e75723ca08e4dd49bb
Sur-vivor <59915404+Sur-vivor@users.noreply.github.com>
2020-10-12 14:59:34 +0000

    pylint: auto fixes

--- userbot/plugins/surcatpfp.py
+++ userbot/plugins/surcatpfp.py
@@ -1,15 +1,13 @@
-#By SurV_ivor
+# By SurV_ivor
 # Set SUR_PIC and SUR_USERNAME in heroku Var
 
 import asyncio
 import os
-import random
 import shutil
 from datetime import datetime
 
 from PIL import Image, ImageDraw, ImageFont
 from pySmartDL import SmartDL
-from telethon.errors import FloodWaitError
 from telethon.tl import functions
 
 from userbot.utils import admin_cmd
@@ -18,19 +16,18 @@ FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf"
 
 SUR_PIC = os.environ.get("SUR_PIC", None)
 if SUR_PIC is None:
-   SUR_PIC = "https://telegra.ph/file/b2cea1712ebaca603e6f4.jpg"
+    SUR_PIC = "https://telegra.ph/file/b2cea1712ebaca603e6f4.jpg"
 else:
-   SUR_PIC = SUR_PIC
+    SUR_PIC = SUR_PIC
 SUR_USERNAME = os.environ.get("SUR_USERNAME", None)
 SUR_USERNAME = str(SUR_USERNAME) if SUR_USERNAME else "SurCat"
+
 
 @borg.on(admin_cmd(pattern="spp$"))
 async def autopic(event):
     await event.edit(f"Autopic by @SurV_ivor")
     downloaded_file_name = "userbot/original_pic.png"
-    downloader = SmartDL(
-        SUR_PIC, downloaded_file_name, progress_bar=False
-    )
+    downloader = SmartDL(SUR_PIC, downloaded_file_name, progress_bar=False)
     downloader.start(blocking=False)
     photo = "userbot/photo_pfp.png"
     while not downloader.isFinished():
@@ -49,8 +46,9 @@ async def autopic(event):
         try:
             await event.client(
                 functions.photos.DeletePhotosRequest(
-                    await event.client.get_profile_photos("me", limit=1))
+                    await event.client.get_profile_photos("me", limit=1)
                 )
+            )
             await event.client(
                 functions.photos.UploadProfilePhotoRequest(file)  # pylint:disable=E0602
             )
