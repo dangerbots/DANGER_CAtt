e6330110984ca9f3af3ed625823183e50d312763
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-10-14 00:49:21 +0530

    Update fedutl.py

--- userbot/plugins/fedutl.py
+++ userbot/plugins/fedutl.py
@@ -41,10 +41,7 @@ async def _(event):
                 if "file" in fedstat.text:
                     await fedstat.click(0)
                     reply = await conv.get_response()
-                    if file:
-                        await borg.forward_messages(event.chat_id, reply)
-                    else:
-                        await borg.send_message(event.chat_id, reply.text)
+                    await borg.forward_messages(event.chat_id, reply)                    
                 else:
                     await borg.send_message(event.chat_id, fedstat.text)
                 await event.delete()
@@ -60,10 +57,7 @@ async def _(event):
                 if "file" in fedstat.text:
                     await fedstat.click(0)
                     reply = await conv.get_response()
-                    if "minutes" in reply.text:
-                        await borg.send_message(event.chat_id, reply.text)
-                    else:
-                        await borg.forward_messages(event.chat_id, reply)
+                    await borg.forward_messages(event.chat_id, reply.text)
                 else:
                     await borg.send_message(event.chat_id, fedstat.text)
                 await event.delete()
