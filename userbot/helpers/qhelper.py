4f0bbcb2cc545e66a66df9625f1d3eedc6bbc2c3
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-10-18 13:37:11 +0530

    Update qhelper.py

--- userbot/helpers/qhelper.py
+++ userbot/helpers/qhelper.py
@@ -1,4 +1,4 @@
-This file is part of NiceGrill.
+#    This file is part of NiceGrill.
 #    NiceGrill is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
