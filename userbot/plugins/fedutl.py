6d9da1430a1f2a617297fa7b979f3443cd5987b5
Sur-vivor <59915404+Sur-vivor@users.noreply.github.com>
2020-10-20 17:31:11 +0000

    pylint: auto fixes

--- userbot/plugins/fedutl.py
+++ userbot/plugins/fedutl.py
@@ -163,8 +163,8 @@ async def get_users(show):
         mentions = "id,reason"
         try:
             if not show.pattern_match.group(1):
-                async for user in show.client.iter_participants(show.chat_id):                                         
-                    if not user.deleted:                 
+                async for user in show.client.iter_participants(show.chat_id):
+                    if not user.deleted:
                         mentions += f"\n{user.id},⚠️Porn / Porn Group Member//AntiPornFed #Massban🔞🛑"
                     elif user.id != bot.uid:
                         mentions += f"\n{user.id},⚠️Porn / Porn Group Member//AntiPornFed #Massban🔞🛑"
