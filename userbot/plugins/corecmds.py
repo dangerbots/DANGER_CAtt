f0a7cb4d3f23f1625b5c17e41353a6c59e0a55ec
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-11-05 19:25:20 +0530

    Update corecmds.py

--- userbot/plugins/corecmds.py
+++ userbot/plugins/corecmds.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from ..utils import admin_cmd, edit_or_reply, load_module, remove_plugin, sudo_cmd
 from . import hmention
 
+
 DELETE_TIMEOUT = 5
 thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
 
@@ -72,7 +73,8 @@ async def send(event):
         ms = (end - start).seconds
         await event.delete()
         await caat.edit(
-            f"__**➥ Plugin Name:- {input_str} .**__\n__**➥ Uploaded in {ms} seconds.**__\n__**➥ Uploaded by :-**__ {hmention}"
+            f"<b><i>➥ Plugin Name :- {input_str} .</i></b>\n<b><i>➥ Uploaded in {ms} seconds.</i></b>\n<b><i>➥ Uploaded by :- {hmention}</i></b>",
+            parse_mode="html",
         )
     else:
         await edit_or_reply(event, "404: File Not Found")
