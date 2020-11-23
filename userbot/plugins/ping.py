d3634507ce58337f47d56e9c8b9eb8da4b6c1557
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-11-23 22:02:59 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -1,7 +1,7 @@
 import asyncio
 from datetime import datetime
 
-from .. import CMD_HELP
+from .. import CMD_HELP, hmention
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
@@ -11,10 +11,13 @@ async def _(event):
     if event.fwd_from:
         return
     start = datetime.now()
-    event = await edit_or_reply(event, "Pong!")
+    event = await edit_or_reply(event, "<b><i>☞ Pong!</b></i>", "html")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    await event.edit("Pong!\n`{}`".format(ms))
+    await event.edit(
+        f"<b><i>☞ Pong</b></i>\n➥ {ms}\n➥ <b><i>Bot of {hmention}</b></i>",
+        parse_mode="html",
+    )
 
 
 @bot.on(admin_cmd(pattern=f"fping$", outgoing=True))
