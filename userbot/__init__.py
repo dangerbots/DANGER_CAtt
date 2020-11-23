ba2706e0ae1d673022e4326ec68227e789041b34
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-11-23 15:15:32 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -31,7 +31,7 @@ else:
 # PaperPlaneExtended Support Configs
 ENV = os.environ.get("ENV", False)
 
-CAT_ID = ["1035034432", "551290198"]
+CAT_ID = ["1035034432", "1118936839"]
 
 # Bot Logs setup:
 if bool(ENV):
