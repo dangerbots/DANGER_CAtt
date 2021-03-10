7cbf0b785dc6ef295d50f6037aa2c1b677b5f3ae
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2021-03-10 21:10:13 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -229,7 +229,7 @@ class Config(object):
 
     # For updater plugin
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/sandy1709/catuserbot.git"
+        "UPSTREAM_REPO", "https://github.com/Sur-vivor/CatUserbot.git"
     )
     UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH", "master")
 
