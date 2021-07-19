5dd19a96225a0298af61b78f2b297c42c24b4889
dangerbots <79362039+dangerbots@users.noreply.github.com>
2021-07-19 21:02:02 +0530

    Update git.py

--- userbot/plugins/git.py
+++ userbot/plugins/git.py
@@ -35,8 +35,8 @@ async def source(e):
     "Source code link of userbot"
     await edit_or_reply(
         e,
-        "Click [here](https://github.com/Sur-vivor/CatUserbot) to open this bot source code\
-        \nClick [here](https://github.com/Sur-vivor/nekopack) to open supported link for heroku",
+        "Click [here](https://github.com/dangerbots/DANGER_CAT) to open this bot source code\
+        \nClick [here](https://github.com/dangerbots/catpack) to open supported link for heroku",
     )
 
 
