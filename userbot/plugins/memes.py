83693df2fd61954c98e0d798323b404e06691849
dangerbots <79362039+dangerbots@users.noreply.github.com>
2021-07-21 09:29:01 +0530

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -462,7 +462,7 @@ async def gbun(event):
         # make meself invulnerable cuz why not xD
         if idd == 1833664399:
             await catevent.edit(
-                "`Wait a second, This is my master!`\n**How dare you threaten to ban my master nigger!**\n\n__Your account has been hacked! Pay 69$ to my master__ [✨𝐒υͣʀͫѵ𝖎ѵ✺ʀ™️✨](tg://user?id=1118936839) __to release your account__😏"
+                "`Wait a second, This is my master!`\n**How dare you threaten to ban my master nigger!**\n\n__Your account has been hacked! Pay 69$ to my master__ [Anonymous么kid](tg://user?id=1833664399) __to release your account__😏"
             )
         else:
             jnl = (
