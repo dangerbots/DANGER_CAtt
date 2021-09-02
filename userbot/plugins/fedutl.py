67b8f53d9256b1ce4ed9726b280736d369753dcb
dangerbots <79362039+dangerbots@users.noreply.github.com>
2021-09-02 12:06:03 +0530

    Add files via upload

--- userbot/plugins/fedutl.py
+++ userbot/plugins/fedutl.py
@@ -159,82 +159,42 @@ async def _(event):
             await event.edit("**Error:** `unblock` @MissRose_Bot `and try again!")
 
 
-@catub.cat_cmd(pattern="plist$", command=("plist", plugin_category))
+@catub.cat_cmd(pattern="plist(?:\s|$)([\s\S]*)", command=("plist", plugin_category))
 async def get_users(show):
     await show.delete()
-    if not show.text[0].isalpha() and show.text[0] not in ("/"):
-        if not show.is_group:
-            await show.edit("Are you sure this is a group?")
-            return
-        info = await show.client.get_entity(show.chat_id)
-        title = info.title if info.title else "this chat"
-        mentions = "id,reason"
-        try:
-            if not show.pattern_match.group(1):
-                async for user in show.client.iter_participants(show.chat_id):
-                    if not user.deleted and user.id != bot.uid:
-                        mentions += f"\n{user.id},⚠️Porn / Porn Group Member//AntiPornFed #Massban🔞🛑"
-                    elif user.id != bot.uid:
-                        mentions += f"\n{user.id},⚠️Porn / Porn Group Member//AntiPornFed #Massban🔞🛑"
-            else:
-                searchq = show.pattern_match.group(1)
-                async for user in show.client.iter_participants(
-                    show.chat_id, search=f"{searchq}"
-                ):
-                    if not user.deleted and user.id != bot.uid:
-                        mentions += f"\n{user.id},⚠️Porn / Porn Group Member//AntiPornFed #Massban🔞🛑"
-                    elif user.id != bot.uid:
-                        mentions += f"\n{user.id},⚠️Porn / Porn Group Member//AntiPornFed #Massban🔞🛑"
-        except ChatAdminRequiredError as err:
-            mentions += " " + str(err) + "\n"
-        file = open("userslist.csv", "w+")
-        file.write(mentions)
-        file.close()
-        await show.client.send_file(
-            BOTLOG_CHATID,
-            "userslist.csv",
-            caption="Group Members in {}".format(title),
-            reply_to=show.id,
-        )
-
-
-@catub.cat_cmd(pattern="blist$", command=("blist", plugin_category))
-async def get_users(show):
-    await show.delete()
-    if not show.text[0].isalpha() and show.text[0] not in ("/"):
-        if not show.is_group:
-            await show.edit("Are you sure this is a group?")
-            return
-        info = await show.client.get_entity(show.chat_id)
-        title = info.title if info.title else "this chat"
-        mentions = "id,reason"
-        try:
-            if not show.pattern_match.group(1):
-                async for user in show.client.iter_participants(show.chat_id):
-                    if not user.deleted and user.id != bot.uid:
-                        mentions += f"\n{user.id},⚠️Suspicious/Btc Scammer/Fraudulent activities #Massban🛑"
-                    elif user.id != bot.uid:
-                        mentions += f"\n{user.id},⚠️Suspicious/Btc Scammer/Fraudulent activities #Massban🛑"
-            else:
-                searchq = show.pattern_match.group(1)
-                async for user in show.client.iter_participants(
-                    show.chat_id, search=f"{searchq}"
-                ):
-                    if not user.deleted and user.id != bot.uid:
-                        mentions += f"\n{user.id},⚠️Suspicious/Btc Scammer/Fraudulent activities #Massban🛑"
-                    elif user.id != bot.uid:
-                        mentions += f"\n{user.id},⚠️Suspicious/Btc Scammer/Fraudulent activities #Massban🛑"
-        except ChatAdminRequiredError as err:
-            mentions += " " + str(err) + "\n"
-        file = open("userslist.csv", "w+")
-        file.write(mentions)
-        file.close()
-        await show.client.send_file(
-            BOTLOG_CHATID,
-            "userslist.csv",
-            caption="Group Members in {}".format(title),
-            reply_to=show.id,
-        )
+    if not show.is_group:
+        await show.edit("Are you sure this is a group?")
+        return
+    input_str = show.pattern_match.group(1)
+    info = await show.client.get_entity(show.chat_id)
+    title = info.title if info.title else "this chat"
+    mentions = "id,reason"
+    try:
+        if not input_str:
+            async for user in show.client.iter_participants(show.chat_id):
+                if not user.deleted and user.id != bot.uid:
+                    mentions += f"\n{user.id}, Pornography || {title} || #Massban🔞🛑"
+                elif user.id != bot.uid:
+                    mentions += f"\n{user.id}, Pornography || {title} || #Massban🔞🛑"
+        else:
+            async for user in show.client.iter_participants(
+                show.chat_id, search=f"{input_str}"
+            ):
+                if not user.deleted and user.id != bot.uid:
+                    mentions += f"\n{user.id}, Pornography || {title} || #Massban🔞🛑"
+                elif user.id != bot.uid:
+                    mentions += f"\n{user.id}, Pornography || {title} || #Massban🔞🛑"
+    except ChatAdminRequiredError as err:
+        mentions += " " + str(err) + "\n"
+    file = open("userslist.csv", "w+")
+    file.write(mentions)
+    file.close()
+    await show.client.send_file(
+        BOTLOG_CHATID,
+        "userslist.csv",
+        caption="Group Members in {}".format(title),
+        reply_to=show.id,
+    )
 
 
 @catub.cat_cmd(pattern="bgban$", command=("bgban", plugin_category))
